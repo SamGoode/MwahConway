dee2715142db2acf97195de434a86189821819cb
Jevan Soh <samgoode1523@gmail.com>
2024-05-01 17:01:23 +1000

    prototype finished

--- MwahConway/MwahConway.cpp
+++ MwahConway/MwahConway.cpp
@@ -14,14 +14,19 @@ private:
     int y;
     int width;
     int height;
-    bool cells[20][20];
+    bool** cells;
 
 public:
     CellAuto() {
         x = 0;
         y = 0;
-        width = 20;
-        height = 20;
+        width = 0;
+        height = 0;
+
+        cells = new bool*[height];
+        for (int i = 0; i < height; i++) {
+            cells[i] = new bool[width];
+        }
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -36,6 +41,11 @@ public:
         width = cellAuto.width;
         height = cellAuto.height;
 
+        cells = new bool*[height];
+        for (int i = 0; i < height; i++) {
+            cells[i] = new bool[width];
+        }
+
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 cells[i][j] = cellAuto.cells[i][j];
@@ -43,17 +53,29 @@ public:
         }
     }
 
-    CellAuto(int x, int y) {
+    CellAuto(int x, int y, int width, int height) {
         this->x = x;
         this->y = y;
-        width = 20;
-        height = 20;
+        this->width = width;
+        this->height = height;
+
+        cells = new bool*[height];
+        for (int i = 0; i < height; i++) {
+            cells[i] = new bool[width];
+        }
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 cells[i][j] = false;
             }
         }
+    }
+
+    ~CellAuto() {
+        for (int i = 0; i < height; i++) {
+            delete[] cells[i];
+        }
+        delete[] cells;
     }
 
     //returns {-1, -1} if out of cell grid
@@ -65,12 +87,84 @@ public:
         return { (mouseX - x) / 2, mouseY - y };
     }
 
-    void toggleCell(int cellPosX, int cellPosY) {
-        cells[cellPosY][cellPosX] = !cells[cellPosY][cellPosX];
+    bool toggleCell(int gridPosX, int gridPosY) {
+        if (gridPosX < 0 || gridPosY < 0) {
+            return false;
+        }
+
+        cells[gridPosY][gridPosX] = !cells[gridPosY][gridPosX];
+        return true;
     }
 
-    void toggleCell(pos cellPos) {
-        cells[cellPos.y][cellPos.x] = !cells[cellPos.y][cellPos.x];
+    bool toggleCell(pos gridPos) {
+        if (gridPos.x < 0 || gridPos.y < 0) {
+            return false;
+        }
+
+        cells[gridPos.y][gridPos.x] = !cells[gridPos.y][gridPos.x];
+        return true;
+    }
+
+    //returns -1 if gridPos is out of cell grid bounds
+    int getCellState(pos gridPos) {
+        if (gridPos.x < 0 || gridPos.x > (width - 1) || gridPos.y < 0 || gridPos.y > (height - 1)) {
+            return -1;
+        }
+
+        return cells[gridPos.y][gridPos.x];
+    }
+
+    int getNeighboursAlive(pos gridPos) {
+        int neighboursAlive = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+
+                if (getCellState({ (gridPos.x - 1) + j, (gridPos.y - 1) + i }) == 1) {
+                    neighboursAlive++;
+                }
+            }
+        }
+
+        return neighboursAlive;
+    }
+
+    void updateCells() {
+        bool** newCells;
+        newCells = new bool*[height];
+        for (int i = 0; i < height; i++) {
+            newCells[i] = new bool[width];
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!cells[i][j]) {
+                    if (getNeighboursAlive({ j, i }) == 3) {
+                        newCells[i][j] = true;
+                        continue;
+                    }
+
+                    newCells[i][j] = false;
+                    continue;
+                }
+
+                if (getNeighboursAlive({ j, i }) < 2 || getNeighboursAlive({ j, i }) > 3) {
+                    newCells[i][j] = false;
+                    continue;
+                }
+                
+                newCells[i][j] = true;
+            }
+        }
+
+        for (int i = 0; i < height; i++) {
+            delete[] cells[i];
+        }
+        delete[] cells;
+
+        cells = newCells;
     }
 
     void display(Screen& screen) {
@@ -128,9 +222,10 @@ int main() {
 
     int mouseX = 0;
     int mouseY = 0;
-    
-    CellAuto cellAuto = CellAuto(50, 10);
-    cellAuto.toggleCell(4, 1);
+
+    CellAuto cellAuto = CellAuto(50, 5, 40, 40);
+
+    int steps = 0;
 
     while (true) {
         GetNumberOfConsoleInputEvents(hin, &events);
@@ -138,14 +233,25 @@ int main() {
         if (events > 0) {
             ReadConsoleInput(hin, &inputRecord, 1, &events);
             switch (inputRecord.EventType) {
-            case MOUSE_EVENT:
-                mouseX = inputRecord.Event.MouseEvent.dwMousePosition.X;
-                mouseY = inputRecord.Event.MouseEvent.dwMousePosition.Y;
+                case KEY_EVENT:
+                    switch (inputRecord.Event.KeyEvent.wVirtualKeyCode) {
+                        case VK_SPACE:
+                            if (inputRecord.Event.KeyEvent.bKeyDown) {
+                                cellAuto.updateCells();
+                                steps++;
+                            }
+                            break;
+                    }
+                    break;
 
-                if (inputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
-                    cellAuto.toggleCell(cellAuto.getMouseGridPos(mouseX, mouseY));
-                }
-                break;
+                case MOUSE_EVENT:
+                    mouseX = inputRecord.Event.MouseEvent.dwMousePosition.X;
+                    mouseY = inputRecord.Event.MouseEvent.dwMousePosition.Y;
+
+                    if (inputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
+                        cellAuto.toggleCell(cellAuto.getMouseGridPos(mouseX, mouseY));
+                    }
+                    break;
             }
         }
 
@@ -158,7 +264,11 @@ int main() {
 
         pos gridPos = cellAuto.getMouseGridPos(mouseX, mouseY);
         screen.text("Mouse screen pos: x:" + std::to_string(mouseX) + ", y:" + std::to_string(mouseY), 2, 1);
-        screen.text("Mouse grid pos: x:" + std::to_string(gridPos.x) + ", y:" + std::to_string(gridPos.y), 2, 3);
+        screen.text("Mouse grid pos: x:" + std::to_string(gridPos.x) + ", y:" + std::to_string(gridPos.y), 2, 2);
+        
+        screen.text("Steps: " + std::to_string(steps), 2, 5);
+        screen.text("Hovered cell state: " + std::to_string(cellAuto.getCellState(cellAuto.getMouseGridPos(mouseX, mouseY))), 2, 7);
+        screen.text("Hovered cell neighbours alive: " + std::to_string(cellAuto.getNeighboursAlive(cellAuto.getMouseGridPos(mouseX, mouseY))), 2, 8);
 
         screen.print();
 
